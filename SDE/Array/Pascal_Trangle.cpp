d07947ed5dd2b45e94fe2f211ffe3dd3b75d67d4
mosfetnik <adityarai424@gmail.com>
2025-04-23 17:27:47 +0530

    SDE sheet added

---
+++ SDE/Array/Pascal_Trangle.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    vector<vector<int>> generate(int numRows)
+    {
+
+        vector<vector<int>> result;
+        vector<int> prevRow;
+
+        for (int i = 0; i < numRows; i++)
+        {
+            vector<int> currRow(i + 1, 1);
+
+            for (int j = 1; j < i; j++)
+            {
+                currRow[j] = prevRow[j - 1] + prevRow[j];
+            }
+            result.push_back(currRow);
+            prevRow = currRow;
+        }
+        return result;
+    }
+};
